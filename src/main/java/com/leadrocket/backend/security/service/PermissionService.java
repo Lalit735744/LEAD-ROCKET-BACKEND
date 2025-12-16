027320a272f69908a4a3e609c9e9714c28fe35cc
lalitkumar94681 <lalitkumar94681>
2025-12-16 16:00:10 +0530

    start_working-created_back_infra

---
+++ src/main/java/com/leadrocket/backend/security/service/PermissionService.java
@@ -0,0 +1,26 @@
+package com.leadrocket.backend.security.service;
+
+import com.leadrocket.backend.users.model.Permission;
+import com.leadrocket.backend.security.repository.PermissionRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SecurityPermissionService { }
+
+	private final PermissionRepository repository;
+
+	public SecurityPermissionService(PermissionRepository repository) {
+		this.repository = repository;
+	}
+
+	public List<Permission> getAll() {
+		return repository.findAll();
+	}
+
+	public Permission create(Permission permission) {
+		return repository.save(permission);
+	}
+}
+
